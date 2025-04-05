2ecf0fa3581e1e820c63adc5baefb527b2ef186d
Manasa M R <manasa.31198ravi@gmail.com>
2025-04-05 19:27:06 +0530

    Error

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ import calculator
 
 class TestCalculator(unittest.TestCase):
        def test_add(self):
-           self.assertEqual(calculator.add(2, 3), 5)
+           self.assertEqual(calculator.add(4, 3), 5)
 
        def test_subtract(self):
            self.assertEqual(calculator.subtract(5, 2), 3)
