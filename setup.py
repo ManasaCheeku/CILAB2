9581531893c6b6b6ef370fc275108aa4b84549d3
Manasa M R <79912831+ManasaCheeku@users.noreply.github.com>
2025-04-20 18:42:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-
+from setuptools import setup
+setup(
+name='calculator',
+version='1.0.0',
+py_modules=['calculator'],
+install_requires=[],
+entry_points={
+'console_scripts': [
+'calculator=calculator:main',
+],
+},
+)
